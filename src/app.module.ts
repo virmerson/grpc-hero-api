de8f93141c202b8e573c1d3f4b8fbb7875cdb55d
Virmerson <virmerson.santos@advanceworks.ai>
2024-10-05 23:48:07 +0100

    fix: correct versio of common-hero-package

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,12 @@
 
 import { Module } from '@nestjs/common';
 import { HeroModule } from './hero/hero.module';
-import { DatabaseModule } from 'common-hero-package';
-import { MongooseModule } from '@nestjs/mongoose';
+import { DatabaseModule } from "common-hero-package"
 
 @Module({
   imports: [
     HeroModule,
-    MongooseModule.forRootAsync({
-      useFactory: () => ({
-        uri: "mongodb://localhost:27017/hero", // Use environment variable
-      }),
-    }),
+    DatabaseModule
   ],
   controllers: [],
   providers: [],
